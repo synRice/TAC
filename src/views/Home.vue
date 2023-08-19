c6e1b4d800a50ea982b9cebb9a4d5d0a20589d08
“buxingo” <294682340@qq.com>
2023-08-19 18:49:28 +0800

    feat: 增加测试页面

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="title">{{ msg }}</h1>
+  <button @click="plusCount">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const msg = ref("hello")
+const count = ref(0)
+
+const plusCount = () => {
+  count.value++;
+}
+</script>
+
+<style lan="sass" scoped>
+.title{
+  font-size: 18px;
+}
+</style>
