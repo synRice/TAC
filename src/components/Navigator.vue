75d672a73ffea61ee09c862bb4030df1658f668f
“buxingo” <294682340@qq.com>
2023-08-28 21:27:38 +0800

    feat: 组件化顶部菜单栏显示

---
+++ src/components/Navigator.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="top-box">
+      <div class="title-icon" @click="backToHome">TAC</div>
+      <div class="navigator" v-for="item in navigatorList" :key=item.name @click="goPage(item.path)">
+        <div class="icon home" :class="item.icon"></div>
+        <span>{{item.name}}</span>
+      </div>
+    </div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+import { navigatorList } from "@/constant/main";
+const router = useRouter()
+const backToHome = () => {
+  router.push({
+    path: '/'
+  })
+}
+
+const goPage = (path) => {
+  router.push({
+    path: '/' + path
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+.top-box{
+  padding: 0 40px;
+  height: 70px;
+  width: 100%;
+  background-color: #fc912a;
+  display: flex;
+  align-items: center;
+  .title-icon{
+    margin-right: 30px;
+    line-height: 40px;
+    font-size: 40px;
+    font-weight: 500;
+    color: #000000;
+    cursor: pointer;
+  }
+  .navigator{
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+    height: 100%;
+    font-size: 20px;
+    cursor: pointer;
+    transition: all .5s ease-out;
+    .icon{
+      width: 20px;
+      height: 20px;
+      margin-right: 4px;
+    }
+    .home{
+      background: no-repeat url("@/assets/img/home.svg");
+    }
+    &:hover{
+      opacity: 0.7;
+      background: rgba(245, 65, 68, 0.2);
+    }
+  }
+}
+</style>
---
+++ src/constant/main.js
@@ -0,0 +1,27 @@
+export const navigatorList = [
+    {
+        name: 'Home',
+        path: 'home',
+        icon: 'home'
+    }, {
+        name: 'Server',
+        path: 'server',
+        icon: 'home'
+    }, {
+        name: 'Task List',
+        path: 'taskList',
+        icon: 'home'
+    }, {
+        name: 'Tutorial',
+        path: 'Tutorial',
+        icon: 'home'
+    }, {
+        name: "What's new",
+        path: 'new',
+        icon: 'home'
+    }, {
+        name: 'Reference',
+        path: 'reference',
+        icon: 'home'
+    }
+]
