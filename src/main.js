6614309bb1756b8f8542fa6b2fa356c7f379c7b4
“buxingo” <294682340@qq.com>
2023-08-28 21:27:08 +0800

    chore: 增加 antd 定义

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
 const app = createApp(App)
 app.use(router)
+app.use(Antd)
 app.mount('#app')
